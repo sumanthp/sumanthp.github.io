48b2a6b510b653267826f7363005dfb92e96054a
sumanthp <sumanth472@gmail.com>
2021-10-24 16:40:38 +0530

    Updates

---
+++ precache-manifest.2e5030c8bab9b7ba674f8060ef0226a4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9be2c708db9359167618967242143f9",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "d661a59a6fbf3f5dbee2",
+    "url": "/home/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/home/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d661a59a6fbf3f5dbee2",
+    "url": "/home/static/js/main.7771f682.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "7d7fb215b3e6b247df2afec78f810ed0",
+    "url": "/home/static/media/mcafee.7d7fb215.png"
+  },
+  {
+    "revision": "0d6d063ab2385ee6a1c02c8e76313ecc",
+    "url": "/home/static/media/sumanthpolisetty.0d6d063a.pdf"
+  },
+  {
+    "revision": "135caca5dbcd9d937bec42f1a849f21f",
+    "url": "/home/static/media/sumanthpolisetty.135caca5.jpg"
+  },
+  {
+    "revision": "9d3e5adf36887a710ed763c7cf165868",
+    "url": "/home/static/media/tavant.9d3e5adf.jpg"
+  }
+]);
